50dcd8f3f613618d9ca168a3ee98e2f792830bb5
borulchenko <ruslan.borulchenko@gmail.com>
2019-06-27 20:49:07 +0300

    174 - Understanding Operators

--- section13/obs-01-start/src/app/home/home.component.ts
+++ section13/obs-01-start/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -26,7 +27,6 @@ export class HomeComponent implements OnInit, OnDestroy {
         if (count === 5) {
           observer.complete();
         }
-
         if (count > 3) {
           observer.error(new Error('Count is greater then 3'));
         }
@@ -34,13 +34,19 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    customIntervalObservable.subscribe(data => {
-      console.log(data);
-    }, error => {
-      alert(error.message);
-    }, () => {
-      console.log('Completed!');
-    });
+
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round ' + (data + 1);
+    }))
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        alert(error.message);
+      }, () => {
+        console.log('Completed!');
+      });
   }
 
   ngOnDestroy(): void {
