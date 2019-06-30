3380b128ad19361966301e62b81aba3daee00b7a
borulchenko <ruslan.borulchenko@gmail.com>
2019-06-30 21:20:16 +0300

    256 - Outputting Posts

--- section18/http-01-start/src/app/app.component.ts
+++ section18/http-01-start/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post [] = [];
 
   constructor(private http: HttpClient) {
   }
@@ -50,8 +50,8 @@ export class AppComponent implements OnInit {
         }
         return postsArray;
       }))
-      .subscribe(response => {
-        console.log(response[0].title);
+      .subscribe(posts => {
+        this.loadedPosts = posts;
       });
   }
 }
