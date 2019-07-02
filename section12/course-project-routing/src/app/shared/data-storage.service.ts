aeaebe5c506b3152283376c4f3cd36e5c672b5e5
borulchenko <ruslan.borulchenko@gmail.com>
2019-07-02 21:20:36 +0300

    279 - Storing Recipes

--- section12/course-project-routing/src/app/shared/data-storage.service.ts
+++ section12/course-project-routing/src/app/shared/data-storage.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+              private recipeService: RecipeService) {
+  }
+
+  saveRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http.put('https://ng-course-recipe-book-da7d2.firebaseio.com/recipes.json', recipes)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
 }
