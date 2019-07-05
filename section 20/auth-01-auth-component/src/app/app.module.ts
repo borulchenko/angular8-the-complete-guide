cd42ab05868ac46857e68a1b3b6c02da5d7417bc
Ruslan Borulchenko <rborulchenko@benefitfocus.com>
2019-07-05 19:32:08 +0300

    313: Understanding entryComponents

--- section 20/auth-01-auth-component/src/app/app.module.ts
+++ section 20/auth-01-auth-component/src/app/app.module.ts
@@ -56,7 +56,10 @@ import { PlaceholderDirective } from './shared/placeholder/placeholder.directive
       useClass: AuthInterceptorService,
       multi: true
     }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AlertComponent
+  ]
 })
 export class AppModule {
 }
