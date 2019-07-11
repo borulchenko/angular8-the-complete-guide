a26b33a750462e5f5c7d48c91a0816947bec7423
borulchenko <ruslan.borulchenko@gmail.com>
2019-07-12 01:35:33 +0300

    334 - Loading Services Differently

--- section 20/auth-01-auth-component/src/app/app.module.ts
+++ section 20/auth-01-auth-component/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { HttpClientModule } from '@angular/common/http';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
     SharedModule,
     CoreModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // providers: [LoggingService]
 })
 export class AppModule {
 }
