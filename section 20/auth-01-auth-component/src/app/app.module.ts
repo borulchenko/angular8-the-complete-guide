ed92813dba3546a8fdca01e67a5cda592f501cb4
borulchenko <ruslan.borulchenko@gmail.com>
2019-07-11 23:09:58 +0300

    329 - Implementing Lazy Loading

--- section 20/auth-01-auth-component/src/app/app.module.ts
+++ section 20/auth-01-auth-component/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
@@ -18,7 +17,6 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
