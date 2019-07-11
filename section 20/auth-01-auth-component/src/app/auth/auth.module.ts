2951146c5d3289c30ce27de477f09ab9bb4a7959
borulchenko <ruslan.borulchenko@gmail.com>
2019-07-12 01:13:17 +0300

    332 - Preloading Lazy-Loaded Code

--- section 20/auth-01-auth-component/src/app/auth/auth.module.ts
+++ section 20/auth-01-auth-component/src/app/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AuthComponent } from './auth.component';
 import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
