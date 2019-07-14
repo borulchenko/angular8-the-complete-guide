f501537aa9b1d51be6d50214de3f3fe4178d5e0c
borulchenko <ruslan.borulchenko@gmail.com>
2019-07-15 00:45:54 +0300

    362 - Auth Finished (For Now...)

--- section 20/auth-01-auth-component/src/app/auth/auth.guard.ts
+++ section 20/auth-01-auth-component/src/app/auth/auth.guard.ts
@@ -3,13 +3,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { map, take, tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private store: Store<fromApp.AppState>) {
   }
 
   canActivate(
@@ -19,8 +22,11 @@ export class AuthGuard implements CanActivate {
     | UrlTree
     | Promise<boolean | UrlTree>
     | Observable<boolean | UrlTree> {
-    return this.authService.user.pipe(
+    return this.store.select('auth').pipe(
       take(1),
+      map(authState => {
+        return authState.user;
+      }),
       map(user => {
         const isAuth = !!user;
 
