ff90c07138adee1d647f93d52f0964d8ecf5013a
borulchenko <ruslan.borulchenko@gmail.com>
2019-07-16 00:09:16 +0300

    363 - And Important Note on Actions

--- section 20/auth-01-auth-component/src/app/shopping-list/store/shopping-list.reducer.ts
+++ section 20/auth-01-auth-component/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -58,13 +58,13 @@ export function shoppingListReducer(
         editedIngredientIndex: -1,
         editedIngredient: null
       };
-    case 'START_EDIT':
+    case ShoppingListActions.START_EDIT:
       return {
         ...state,
         editedIngredientIndex: action.payload,
         editedIngredient: {...state.ingredients[action.payload]}
       };
-    case 'STOP_EDIT':
+    case ShoppingListActions.STOP_EDIT:
       return {
         ...state,
         editedIngredient: null,
