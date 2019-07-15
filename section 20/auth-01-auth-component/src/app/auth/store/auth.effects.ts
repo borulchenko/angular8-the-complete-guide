e65350239c2b0de22b5a70c34dce90de02481f14
borulchenko <ruslan.borulchenko@gmail.com>
2019-07-16 00:55:13 +0300

    366 - Effects & Error Handling

--- section 20/auth-01-auth-component/src/app/auth/store/auth.effects.ts
+++ section 20/auth-01-auth-component/src/app/auth/store/auth.effects.ts
@@ -1,12 +1,46 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import * as AuthActions from './auth.actions';
+import { environment } from '../../../environments/environment';
+import { of } from 'rxjs';
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: string;
+}
 
 export class AuthEffects {
-  private authLogin = this.actions$.pipe(
-    ofType(AuthActions.LOGIN_START)
+  @Effect()
+  authLogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http
+        .post<AuthResponseData>(
+          'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + environment.firebaseAPIKey,
+          {
+            email: authData.payload.email,
+            password: authData.payload.password,
+            returnSecureToken: true
+          }
+        ).pipe(
+          catchError(error => {
+            of();
+          }),
+          map(respData => {
+            of();
+          }));
+    }),
+
   );
 
   // add dollar sign to the Observables
-  constructor(private actions$: Actions) {
+  constructor(private actions$: Actions,
+              private http: HttpClient) {
   }
 }
