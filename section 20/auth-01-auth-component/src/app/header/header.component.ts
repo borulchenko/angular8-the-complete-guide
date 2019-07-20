18d1fb4feee78ae68af2270701d89914c732fb34
borulchenko <ruslan.borulchenko@gmail.com>
2019-07-20 17:31:38 +0300

    380 - Fetching Recipes & Using the Resolver

--- section 20/auth-01-auth-component/src/app/header/header.component.ts
+++ section 20/auth-01-auth-component/src/app/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
-import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
-import { map } from 'rxjs/operators';
 import * as AuthActions from '../auth/store/auth.actions';
+import * as RecipeActions from '../recipes/store/recipe.actions';
 
 @Component({
   selector: 'app-header',
@@ -37,7 +39,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   onFetchData() {
-    this.dataStorageService.fetchRecipes().subscribe();
+    this.store.dispatch(new RecipeActions.FetchRecipes());
   }
 
   onLogout() {
--- section 20/auth-01-auth-component/src/app/recipes/store/recipe.effects.ts
+++ section 20/auth-01-auth-component/src/app/recipes/store/recipe.effects.ts
@@ -1,3 +1,36 @@
-export class RecipeEffects {
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, switchMap } from 'rxjs/operators';
 
+import { Recipe } from '../recipe.model';
+import * as RecipeActions from './recipe.actions';
+
+@Injectable()
+export class RecipeEffects {
+  @Effect()
+  fetchRecipes = this.actions$.pipe(
+    ofType(RecipeActions.FETCH_RECIPES),
+    switchMap(() => {
+      return this.http
+        .get<Recipe[]>(
+          'https://ng-course-recipe-book-da7d2.firebaseio.com/recipes.json'
+        );
+    }),
+    map(recipes => {
+      return recipes.map(recipe => {
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients ? recipe.ingredients : []
+        };
+      });
+    }),
+    map(recipes => {
+      return new RecipeActions.SetRecipes(recipes);
+    })
+  );
+
+  constructor(private actions$: Actions,
+              private http: HttpClient) {
+  }
 }
