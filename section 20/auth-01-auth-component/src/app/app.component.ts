50b68c725717e1304cf301babe7cc4aa405fc76c
borulchenko <ruslan.borulchenko@gmail.com>
2019-07-21 14:20:10 +0300

    440 - Adding Angular Universal with NestJS

--- section 20/auth-01-auth-component/src/app/app.component.ts
+++ section 20/auth-01-auth-component/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { Store } from '@ngrx/store';
+
 import { LoggingService } from './logging.service';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
