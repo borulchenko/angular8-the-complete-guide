0a05177d08bf0ec8d81acc6a3d6e8592be7f974d
borulchenko <ruslan.borulchenko@gmail.com>
2019-07-30 08:19:12 +0300

    475 - Simulating Async Tasks

--- section 28/complete-guide-final-webpack/src/app/user/user.component.spec.ts
+++ section 28/complete-guide-final-webpack/src/app/user/user.component.spec.ts
@@ -2,6 +2,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UserComponent} from './user.component';
 import {UserService} from './user.service';
+import {DataService} from '../shared/data.service';
 
 describe('UserComponent', () => {
   beforeEach(() => {
@@ -33,11 +34,33 @@ describe('UserComponent', () => {
     expect(compiled.querySelector('p').textContent).toContain(app.user.name);
   });
 
-  it('should shouldn not display the user name if user is logged in', function() {
+  it('should shouldn\`t display the user name if user is logged in', function() {
     let fixture = TestBed.createComponent(UserComponent);
     let app = fixture.debugElement.componentInstance;
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('p').textContent).not.toContain(app.user.name);
   });
+
+  it('shouldn\`t fetch data succesfully if not called asynchronously', function() {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('Dummy Data'));
+    fixture.detectChanges();
+    expect(app.data).toBe(undefined);
+  });
+
+  it('should fetch data succesfully if called asynchronously', async(function() {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('Dummy Data'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.data).toBe('Dummy Data');
+    });
+  }));
 });
